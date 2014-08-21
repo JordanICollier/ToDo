b343c3cda4551fd43c2d74e7a74aba7764ac655b
Robovirtuoso <alexwilliams4@me.com>
2014-08-20 20:46:14 -0500

    Manage todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 class TodosController < ApplicationController
   def index
+    # TODO Write this method
+    @todos = Todo.unfinished
   end
 
   def show
@@ -13,18 +15,28 @@ class TodosController < ApplicationController
   def create
     todo = Todo.new(todo_params)
     todo.save
-    redirect_to({ action: "show", id: todo.id })
+    redirect_to todos_path
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+    render "new"
   end
 
   def update
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes(todo_params)
+    redirect_to todos_path
   end
 
   def destroy
+    @todo = Todo.find(params[:id]).destroy
+    redirect_to todos_path, flash: { notice: "Successfully deleted: #{@todo.name}" }
   end
 
   private
 
   def todo_params
-    params.require(:todo).permit(:name)
+    params.require(:todo).permit(:name, :status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726144853) do
+ActiveRecord::Schema.define(version: 20140821013238) do
 
   create_table "todos", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140726144853) do
     t.datetime "finished_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
 end
